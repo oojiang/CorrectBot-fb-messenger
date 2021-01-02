6d877070e6fa81c3617d02d34123da1af2e8e63e
Oliver Jiang <ojiang@berkeley.edu>
2021-01-01 21:11:43 -0800

    Added try catch to X-Hub Auth code to ensure fb receives 'ok 200' response

--- app.py
+++ app.py
@@ -31,14 +31,17 @@ def verify():
     
 @app.route('/', methods=['POST'])
 def webhook():
-    header_signature = request.headers.get('X-Hub-Signature')
-    if not header_signature:
-        log('X-Hub-Signature missing')
-        return 'ok', 200
-    expected_signature = hmac.new(APP_SECRET, msg=request.data, digestmod='sha1')
-    if not hmac.compare_digest(str(header_signature.hexdigest()), str(header_signature)):
-        log('X-Hub-Signature mismatch')
-        return 'ok', 200
+    try:
+        header_signature = request.headers.get('X-Hub-Signature')
+        if not header_signature:
+            log('X-Hub-Signature missing')
+        expected_signature = hmac.new(APP_SECRET, msg=request.data, digestmod='sha1')
+        if not hmac.compare_digest(str(header_signature.hexdigest()), str(header_signature)):
+            log('X-Hub-Signature mismatch')
+    catch BaseException:
+        log("X-HUB-SIGNATURE EXCEPTION")
+        send_message(sender_id, "Woops, something went wrong (unless nothing did).")
+    return 'ok', 200
     data = request.get_json()
     if data.get('object') == 'page' and data.get('entry'):
         for entry in data['entry']:
