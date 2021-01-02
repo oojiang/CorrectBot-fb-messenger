8cc43e32b8abe0441190c5994cd1d4cd58a5712d
Oliver Jiang <ojiang@berkeley.edu>
2021-01-01 21:20:04 -0800

    Python uses 'except', not 'catch'

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def webhook():
         expected_signature = hmac.new(APP_SECRET.encode('utf-8'), msg=request.data, digestmod='sha1')
         if not hmac.compare_digest(str(header_signature.hexdigest()), str(header_signature)):
             log('X-Hub-Signature mismatch')
-    catch BaseException:
+    except BaseException:
         log("X-HUB-SIGNATURE EXCEPTION")
         send_message(sender_id, "Woops, something went wrong (unless nothing did).")
     return 'ok', 200
